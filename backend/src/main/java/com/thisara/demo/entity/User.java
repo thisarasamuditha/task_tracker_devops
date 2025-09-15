a5dadaa91ee1ae1d1df1f43d44430b32055148d7
ASUS <thellamburavithanagethisarasam@gmail.com>
2025-09-15 14:07:46 +0530

    created user class and fixed bugs

---
+++ backend/src/main/java/com/thisara/demo/entity/User.java
@@ -0,0 +1,36 @@
+package com.thisara.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")  // Map to the 'user_id' column in the database
+    private Long userId;
+
+    private String username;
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
