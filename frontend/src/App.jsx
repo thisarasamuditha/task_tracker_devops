98a9fe35778491ac22ade8a44408cbf8aa385de6
ASUS <thellamburavithanagethisarasam@gmail.com>
2025-09-15 22:17:50 +0530

    feat: implement routing for login and signup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import './App.css'
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
