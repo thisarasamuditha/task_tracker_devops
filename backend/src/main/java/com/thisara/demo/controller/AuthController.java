4790c67fadc6fc0ab42b98c9faea9ba39cd0b4d0
ASUS <thellamburavithanagethisarasam@gmail.com>
2025-09-15 14:06:04 +0530

    Created and fix bugs in AuthController.java file

---
+++ backend/src/main/java/com/thisara/demo/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.thisara.demo.controller;
+
+import com.thisara.demo.entity.User;
+import com.thisara.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> loginData) {
+        String username = loginData.get("username");
+        String password = loginData.get("password");
+
+        Optional<User> user = userService.authenticate(username, password);
+
+        if (user.isPresent()) {
+            // Here, generate a JWT token (optional) and return to the client
+            String token = "jwt_token"; // This should be generated properly
+            return ResponseEntity.ok(new HashMap<String, String>() {{
+                put("message", "Login successful");
+                put("token", token);
+            }});
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+}
+
