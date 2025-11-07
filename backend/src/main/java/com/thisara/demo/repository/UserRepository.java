8d64a65d6d3f31ef96c0423040942afb49ab87f3
Thisara samuditha <thellamburavithanagethisarasam@gmail.com>
2025-11-07 20:19:08 +0530

    Refactored the UserRepository interface

--- backend/src/main/java/com/thisara/demo/repository/UserRepository.java
+++ backend/src/main/java/com/thisara/demo/repository/UserRepository.java
@@ -1,9 +1,10 @@
-package com.thisara.demo.repository;  // Adjust package name accordingly
+package com.thisara.demo.repository;
 
-import com.thisara.demo.entity.User;  // Import the User entity
+import com.thisara.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
