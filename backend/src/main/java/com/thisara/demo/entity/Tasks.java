303bf01f8a7bae50c46f9a4d5e1a95f0ff8f27f2
Thisara samuditha <thellamburavithanagethisarasam@gmail.com>
2025-11-07 20:18:39 +0530

    Refactored the Task entity

--- backend/src/main/java/com/thisara/demo/entity/Tasks.java
+++ backend/src/main/java/com/thisara/demo/entity/Tasks.java
@@ -38,6 +38,23 @@ public class Tasks {
     @Column(name = "updated_at", insertable = false, updatable = false)
     private LocalDateTime updatedAt;
 
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+
     public String getTitle() {
         return title;
     }
