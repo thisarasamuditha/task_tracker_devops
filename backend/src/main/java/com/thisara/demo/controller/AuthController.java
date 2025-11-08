fa7a21639f1ba5ebdf1156a95f86d4adae4a2fe2
Thisara samuditha <thellamburavithanagethisarasam@gmail.com>
2025-11-08 18:38:20 +0530

    Enable CORS for AuthController and ensure password field is ignored in User entity

--- backend/src/main/java/com/thisara/demo/controller/AuthController.java
+++ backend/src/main/java/com/thisara/demo/controller/AuthController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     @Autowired
--- backend/src/main/java/com/thisara/demo/entity/User.java
+++ backend/src/main/java/com/thisara/demo/entity/User.java
@@ -1,5 +1,6 @@
 package com.thisara.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -12,8 +13,18 @@ public class User {
     private Long userId;
 
     private String username;
+    
+    @JsonIgnore
     private String password;
 
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
     public String getUsername() {
         return username;
